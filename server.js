5e0c21eee09daeecf7686e2a6e5ea6dba748906a
comunicacao2staleksbrasil-wq <comunicacao2.staleksbrasil@gmail.com>
2026-04-08 10:42:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,24 @@ const app = express();
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    const user = req.query.user || localStorage?.getItem('user');
+    if (user) {
+        res.sendFile(path.join(__dirname, 'dashboard.html'));
+    } else {
+        res.sendFile(path.join(__dirname, 'login.html'));
+    }
+});
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+app.get('/dashboard.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dashboard.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'login.html'));
 });
 
 const PORT = process.env.PORT || 3000;
